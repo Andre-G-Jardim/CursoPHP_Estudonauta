011e4bdae7d82f918f5c64c26c4a1b0cfc7aa2a6
André Gonçalves Jardim <andrej@ufcspa.edu.br>
2023-06-29 14:56:07 -0300

    arq php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diretório</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>
+            Menu curso php
+        </h1>
+        <p>Para onde você que ir?</p>
+    </header>
+    <section>
+
+        <h1>Escolha o diretório</h1>
+        <button onclick="javascript:window.location.href='desafios/'">Desafios</button>
+
+        <button onclick="javascript:window.location.href='exercicios/index.html'">Exercícios</button>
+
+        <button onclick="javascript:window.location.reload(true)"><?="\u{1F30E}"?> Recarregar a página</button>
+    </section>
+</body>
+</html>
